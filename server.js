dd4a9590e74579043970bf4c5e7e1c427abe2b7b
ChandraSekhar045 <30967855+ChandraSekhar045@users.noreply.github.com>
2017-09-07 07:54:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-<!doctype html>
-<html>
-    <head>
-       <title> Article-One</title>
-    </head>
-    <body>
-        <div>
-            <a href="/">Home></a>
-        </div>
-        <p>THis is first paragraph of the artice one and it has some random content</p>
-        <p>THis is second paragraph of the artice one and it has some random content same as above</p>
-    </body>
-</html>
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-two', function(req,res) {
+    res.send("This is article two ");
+});
+
+app.get('/article-three', function(req,res) {
+    res.send("This is article three ");
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
