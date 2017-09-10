1a9ce509aa982587c47c7ad5b41fc9b00cc84864
ChandraSekhar045 <30967855+ChandraSekhar045@users.noreply.github.com>
2017-09-10 14:14:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ var articleOne={
     content:"    <p> THis is first paragraph of the artice one and it has some random content </p> "+
    " <p> THis is second paragraph of the artice one and it has some random content same as above </p> "
     };
+    
+var htmlTemplate= 
+" 
+<html>
+    <head>
+       <title>$(title)</title>
+       <meta name="viewport" content="width-device-width, initial scale=1" />
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="container">
+        <div>
+            <a href="/">Home></a>
+        </div>
+        <h3>
+            $(heading)
+        </h3>
+        <div>
+            $(date)
+        </div>
+        $(content)
+    </div>
+    </body>
+</html>
+" ;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
