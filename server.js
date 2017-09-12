d8520e64d6afca1714f9d2672d83198f756e4652
ChandraSekhar045 <30967855+ChandraSekhar045@users.noreply.github.com>
2017-09-12 09:09:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articlename', function(req,res) {
+app.get('/:articleName', function(req,res) {
     //articleName==articleOne
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
